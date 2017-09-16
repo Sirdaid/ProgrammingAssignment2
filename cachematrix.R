61cdcc409657726d9bb995f6d86579b022fd6455
Sirdaid <31406848+Sirdaid@users.noreply.github.com>
2017-09-16 08:29:37 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Indietify potencially memory consuming calculations 
 
-## Write a short comment describing this function
+## makeCacheMatrix create a list containign a function to 
+# 1. set the value of the matrix
+# 2. get the value of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Check if the mean value has been calculated, otherwise calculate it 
+cacheSolve <- function(x=matrix(), ...) {
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
 }
+
+## Call the function 
+a<-makeCacheMatrix()
+## Set the varaible
+a$set(matrix(1:4,2,2))
+## Solve the function
+cacheSolve(a)
